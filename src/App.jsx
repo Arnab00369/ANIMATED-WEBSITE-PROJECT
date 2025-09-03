ff1f4a0bcc8e2a9f5fc2168f681f0d5d694ce184
Arnab Jana <arnab003jn.369@gmail.com>
2025-09-03 18:58:47 +0530

    talwind add done and few modifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 const App =() =>{
   return(
-    <div>App</div>
+    <main>
+    <h1 className="text-5xl text-orange-500 font-bold">Welcome to Awards</h1>
+    </main>
   )
 }
 export default App;
