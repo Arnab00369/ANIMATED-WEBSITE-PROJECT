6219b027e469e4036225b576d513d9245b395b80
Arnab Jana <arnab003jn.369@gmail.com>
2025-09-03 20:33:37 +0530

    Day_01 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import Hero from './components/Hero.jsx';
 const App =() =>{
   return(
-    <main>
-    <h1 className="text-5xl text-orange-500 font-bold">Welcome to Awards</h1>
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero/>
     </main>
   )
 }
